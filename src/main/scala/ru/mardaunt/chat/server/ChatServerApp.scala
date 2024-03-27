f83e2042c76f858dc5246a1c99dcd2414e04cc5e
Mardaunt <Tepeshman@gmail.com>
2024-03-27 16:26:16 +0300

    add chat service

--- src/main/scala/ru/mardaunt/chat/server/ChatServerApp.scala
+++ src/main/scala/ru/mardaunt/chat/server/ChatServerApp.scala
@@ -1,11 +1,30 @@
 package ru.mardaunt.chat.server
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import fs2.concurrent.Topic
+import io.grpc.ServerServiceDefinition
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
+import fs2.grpc.syntax.all._
+import ru.mardaunt.chat.{ChatServiceFs2Grpc, Events}
+
+import java.util.concurrent.TimeUnit
 
 object ChatServerApp extends IOApp {
 
-  override def run(args: List[String]): IO[ExitCode] = {
-    IO(ExitCode.Success)
-  }
+  override def run(args: List[String]): IO[ExitCode] = for {
+    topic <- Topic[IO, Events]
+    serviceResource: Resource[IO, ServerServiceDefinition] =
+      ChatServiceFs2Grpc.bindServiceResource[IO](ChatService(topic))
+    _ <- serviceResource.use(runServer)
+  } yield ExitCode.Success
+
+  private def runServer(service: ServerServiceDefinition): IO[Nothing] =
+    NettyServerBuilder
+      .forPort(60065)
+      .keepAliveTime(5, TimeUnit.SECONDS)
+      .addService(service)
+      .resource[IO]
+      .evalMap(server => IO(server.start))
+      .useForever
 
 }
